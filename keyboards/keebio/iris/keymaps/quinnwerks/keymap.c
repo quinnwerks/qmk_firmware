0efa63ae0498b2918bddfb632352fbcfcb855fb9
quinnwerks <quinn.smith98@gmail.com>
2020-05-09 19:51:41 -0400

    improve keymap

--- keyboards/keebio/iris/keymaps/quinnwerks/keymap.c
+++ keyboards/keebio/iris/keymaps/quinnwerks/keymap.c
@@ -40,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     QW_SFCP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RAISE,            KC_LGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    RAISE,            KC_LGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, QW_RSCT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    QW_LAGI, LOWER,   KC_SPC,                    KC_ENT,  LOWER,   KC_CAPS
+                                    KC_LALT, LOWER,   KC_SPC,                    KC_ENT,  LOWER,   KC_CAPS
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
